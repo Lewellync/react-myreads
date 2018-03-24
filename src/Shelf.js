02f836b6e57ca1a11b06efc31ff1f5cd1fdc0ceb
Connor Lewellyn <lewellync@gmail.com>
2018-03-24 00:20:12 -0400

    feat: Filtering books on shelf
    Filtering books based on shelf, as well as some basic
    passing of props from shelf to child books

--- src/Shelf.js
+++ src/Shelf.js
@@ -9,14 +9,19 @@ class Shelf extends Component {
   }
 
   render() {
+    console.log(this.props.books)
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">{this.props.name}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <Book 
-              //name={this.props.books[0].name}
-            />
+            {this.props.books.map((book) => (
+              <li key={book.id}>
+                <Book
+                  name={book.title}
+                />
+              </li>
+            ))}
           </ol>
         </div>
       </div>
